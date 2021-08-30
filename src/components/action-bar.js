00f8ef75a0a167bae686739d9c0c1d08ad6f7a15
amirzaushnizer <amir.zaushnizer@duda.co>
2021-08-30 20:38:07 +0300

    finish resizing mechanism

--- src/components/action-bar.js
+++ src/components/action-bar.js
@@ -1,26 +1,46 @@
-import React from 'react'
-import Button from 'react-bootstrap/Button';
+import React from "react";
+import Button from "react-bootstrap/Button";
 
 export const ActionBar = () => {
-    const analyzeGridAction = async () => {
-        await setTimeout(() => {
-            console.log(`Analyzed grid...`)
-        }, 3000)
-    }
+  async function loadImage(url, elem) {
+    return new Promise((resolve, reject) => {
+      elem.onload = () => resolve(elem);
+      elem.onerror = reject;
+      elem.src = url;
+    });
+  }
 
-    const cleanGridAction = () => {
-        const canvas = document.getElementById('canvas');
-        const context = canvas.getContext('2d');
-        context.clearRect(0, 0, canvas.width, canvas.height);
-    }
+  const resizeImage = async (image) => {
+    const canvas = document.createElement("canvas");
+    const size = 28;
+    canvas.width = size;
+    canvas.height = size;
+    const ctx = canvas.getContext("2d");
+    const imageContainer = new Image();
+    await loadImage(image, imageContainer);
+    ctx.drawImage(imageContainer, 0, 0, size, size);
+    return ctx
+      .getImageData(0, 0, canvas.width, canvas.height)
+      .data.filter((element, index) => index % 4 === 0);
+  };
 
+  const analyzeGridAction = async () => {
+    const canvas = document.getElementById("canvas");
+    const bigImage = canvas.toDataURL();
+    const smallImage = await resizeImage(bigImage);
+    console.log(smallImage);
+  };
 
+  const cleanGridAction = () => {
+    const canvas = document.getElementById("canvas");
+    const context = canvas.getContext("2d");
+    context.clearRect(0, 0, canvas.width, canvas.height);
+  };
 
-
-    return (
-        <div>
-            <Button onClick={analyzeGridAction}>Analayze Grid</Button>
-            <Button onClick={cleanGridAction}>Clean Grid</Button>
-        </div>
-    )
-}
+  return (
+    <div id="action-bar">
+      <Button onClick={analyzeGridAction}>Analayze Grid</Button>
+      <Button onClick={cleanGridAction}>Clean Grid</Button>
+    </div>
+  );
+};
