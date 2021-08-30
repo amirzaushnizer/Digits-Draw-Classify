9e73a35f35857c9d255e86900c07927043790fb8
amirzaushnizer <amir.zaushnizer@duda.co>
2021-08-30 20:38:39 +0300

    finish resizing

--- src/components/grid.js
+++ src/components/grid.js
@@ -1,59 +1,57 @@
-
-import React from 'react'
-import { useState } from 'react'
+import React from "react";
+import { useState } from "react";
 
 export const Grid = () => {
-    const [isDrawing, setIsDrawing] = useState(false);
-    const [x, setX] = useState(0);
-    const [y, setY] = useState(0);
+  const [isDrawing, setIsDrawing] = useState(false);
+  const [x, setX] = useState(0);
+  const [y, setY] = useState(0);
 
-    const startDrawing = (e) => {
-        setIsDrawing(true)
-        setX(e.nativeEvent.offsetX);
-        setY(e.nativeEvent.offsetY);
+  const startDrawing = (e) => {
+    setIsDrawing(true);
+    setX(e.nativeEvent.offsetX);
+    setY(e.nativeEvent.offsetY);
+  };
+
+  const stopDrawing = (e) => {
+    if (isDrawing) {
+      draw(x, y, e.nativeEvent.offsetX, e.nativeEvent.offsetY);
+      setIsDrawing(false);
+      setX(0);
+      setY(0);
     }
+  };
 
-    const stopDrawing = (e) => {
-        if(isDrawing){
-            draw(x, y, e.nativeEvent.offsetX, e.nativeEvent.offsetY)
-            setIsDrawing(false)
-            setX(0);
-            setY(0);
-        }
+  const onMouseMove = (e) => {
+    if (isDrawing) {
+      draw(x, y, e.nativeEvent.offsetX, e.nativeEvent.offsetY);
+      setX(e.nativeEvent.offsetX);
+      setY(e.nativeEvent.offsetY);
     }
+  };
 
-    const onMouseMove = (e) => {
-        if(isDrawing){
-            draw(x, y, e.nativeEvent.offsetX, e.nativeEvent.offsetY)
-            setX(e.nativeEvent.offsetX);
-            setY(e.nativeEvent.offsetY);
-        }
-    }
+  const draw = (x1, y1, x2, y2) => {
+    const ctx = document.getElementById("canvas").getContext("2d");
+    ctx.beginPath();
+    ctx.strokeStyle = "white";
+    ctx.lineWidth = 15;
+    ctx.lineCap = "round";
+    ctx.moveTo(x1, y1);
+    ctx.lineTo(x2, y2);
+    ctx.stroke();
+    ctx.closePath();
+  };
 
-    const draw = (x1, y1, x2, y2) => {
-        const ctx = document.getElementById('canvas').getContext('2d');
-        ctx.beginPath()
-        ctx.strokeStyle='white'
-        ctx.lineWidth = 15;
-        ctx.lineCap = 'round'
-        ctx.moveTo(x1, y1);
-        ctx.lineTo(x2, y2);
-        ctx.stroke();
-        ctx.closePath()
-    }
-
-    return (
-        <div>
-            <canvas 
-            id="canvas" 
-            width="500"
-            height="500"
-            style={{backgroundColor: 'black'}}
-            onMouseDown={startDrawing}
-            onMouseUp={stopDrawing}
-            onMouseMove={onMouseMove}
-            onClick={(e) => {console.log(`${e.nativeEvent.offsetY}`)}}
-            ></canvas>
-        </div>
-    )
-}
+  return (
+    <div>
+      <canvas
+        id="canvas"
+        width="300px"
+        height="300px"
+        style={{ backgroundColor: "black" }}
+        onMouseDown={startDrawing}
+        onMouseUp={stopDrawing}
+        onMouseMove={onMouseMove}
+      ></canvas>
+    </div>
+  );
+};
